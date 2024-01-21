482eeb1eda8eba28017c2acd3a7376ab6aea8489
Darshan Desale <90020399+darshan9396@users.noreply.github.com>
2024-01-21 19:50:23 +0530

    AddEmployee and AddProduct Added

--- shoppingmall-frontend/src/Pages/AddEmployee.jsx
+++ shoppingmall-frontend/src/Pages/AddEmployee.jsx
@@ -1,15 +1,127 @@
 import Layout from "../Layout/Layout";
+import React, { useState } from 'react';
 
 const AddEmployee = () => {
-    
+  const [formData, setFormData] = useState({
+    employeeName: '',
+    employeeEmail: '',
+    employeePhoneno: '',
+    employeeSalary: '',
+    employeeDesignation: '',
+    employeeJoiningDate: '',
+    employeeAddress: '',
+  });
+
+ 
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('Employee Details saved Successfully:', formData);
+  };
+
   return (
-    <>
-      <Layout>
-    <div>
-        Employee Form
+    <Layout>
+    <div className="container mt-5">
+      <form onSubmit={handleSubmit}>
+        <h3>Employee Form</h3>
+        <div className="mb-3">
+          {/* <label htmlFor="employeeName" className="form-label">Employee Name</label> */}
+          <input
+            type="text"
+            className="form-control"
+            id="employeeName"
+            name="employeeName"
+            value={formData.employeeName}
+            onChange={handleInputChange}
+            placeholder="Enter employee name"
+          />
+        </div>
+      
+        <div className="mb-3">
+          {/* <label htmlFor="employeeEmail" className="form-label">Employee Email</label> */}
+          <input
+            type="email"
+            className="form-control"
+            id="employeeEmail"
+            name="employeeEmail"
+            value={formData.employeeEmail}
+            onChange={handleInputChange}
+            placeholder="Enter employee email"
+          />
+        </div>
+        <div className="mb-3">
+          {/* <label htmlFor="employeeName" className="form-label">Employee Phone no</label> */}
+          <input
+            type="tel"
+            className="form-control"
+            id="employeePhoneno"
+            name="employeePhoneno"
+            value={formData.employeePhoneno}
+            onChange={handleInputChange}
+            placeholder="Enter employee Phone no"
+          />
+        </div>
+        <div className="mb-3">
+          {/* <label htmlFor="employeeSalary" className="form-label">Salary</label> */}
+          <input
+            type="number"
+            className="form-control"
+            id="employeeSalary"
+            name="employeeSalary"
+            value={formData.employeeSalary}
+            onChange={handleInputChange}
+            placeholder="Enter employee salary"
+          />
+        </div>
+        <div className="mb-3">
+          {/* <label htmlFor="employeeDesignation" className="form-label">Designation</label> */}
+          <input
+            type="text"
+            className="form-control"
+            id="employeeDesignation"
+            name="employeeDesignation"
+            value={formData.employeeDesignation}
+            onChange={handleInputChange}
+            placeholder="Enter employee designation"
+          />
+        </div>
+        <div className="mb-3">
+          {/* <label htmlFor="employeeJoiningDate" className="form-label">Date of Joining</label> */}
+          <input
+            type="date"
+            className="form-control"
+            id="employeeJoiningDate"
+            name="employeeJoiningDate"
+            value={formData.employeeJoiningDate}
+            onChange={handleInputChange}
+          />
+        </div>
+        <div className="mb-3">
+          {/* <label htmlFor="employeeAddress" className="form-label">Address</label> */}
+          <input
+            type="text"
+            className="form-control"
+            id="employeeAddress"
+            name="employeeAddress"
+            value={formData.employeeAddress}
+            onChange={handleInputChange}
+            placeholder="Enter employee address"
+          />
+        </div>
+        <button type="submit" className="btn btn-primary btn-center">
+          Submit
+        </button>
+      </form>
     </div>
-      </Layout>
-    </>
+    </Layout>
   );
 };
+
 export default AddEmployee;
--- shoppingmall-frontend/src/Pages/AddProduct.jsx
+++ shoppingmall-frontend/src/Pages/AddProduct.jsx
@@ -1,10 +1,111 @@
 import Layout from "../Layout/Layout";
+import React, { useState } from 'react';
 
 const AddProduct = () => {
+  const [productData, setProductData] = useState({
+    productName: '',
+    category: ['mens','womens','electronics','accessories'],
+    price: '',
+    image: '',
+    description: '',
+  });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setProductData({
+      ...productData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('Product details saved Successfully!', productData);
+  };
+
   return (
-    <>
-      <Layout></Layout>
-    </>
+    <Layout>
+    <div className="container mt-5">
+      <form onSubmit={handleSubmit}>
+        <h2>Product Form</h2>
+        <div className="mb-3">
+          {/* <label htmlFor="productName" className="form-label">
+            Product Name
+          </label> */}
+          <input
+            type="text"
+            className="form-control"
+            id="productName"
+            name="productName"
+            value={productData.productName}
+            onChange={handleInputChange}
+            placeholder="Enter product name"
+          />
+        </div>
+        <div className="mb-3">
+          {/* <label htmlFor="category" className="form-label">
+            Category
+          </label> */}
+          <select
+            className="form-control"
+            id="category"
+            name="category"
+            value={productData.category}
+            onChange={handleInputChange}
+          >
+            <option value="mens">Men's</option>
+            <option value="womens">Women's</option>
+            <option value="electronics">Electronics</option>
+            <option value="accessories">Accessories</option>
+          </select>
+        </div>
+        <div className="mb-3">
+          {/* <label htmlFor="price" className="form-label">
+            Product Price
+          </label> */}
+          <input
+            type="text"
+            pattern="\d+"
+            title="Please enter a valid number"
+            className="form-control"
+            id="price"
+            name="price"
+            value={productData.price}
+            onChange={handleInputChange}
+            placeholder="Enter product price"
+          />
+        </div>
+        <div className="mb-3">
+          {/* <label htmlFor="image" className="form-label">
+            Product Image
+          </label> */}
+          <input
+            type="file"
+            className="form-control"
+            id="image"
+            name="image"
+            onChange={handleInputChange}
+          />
+        </div>
+        <div className="mb-3">
+          {/* <label htmlFor="description" className="form-label">
+            Product Description
+          </label> */}
+          <textarea
+            className="form-control"
+            id="description"
+            name="description"
+            value={productData.description}
+            onChange={handleInputChange}
+            placeholder="Enter product description"
+          ></textarea>
+        </div>
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </form>
+    </div>
+    </Layout>
   );
 };
 export default AddProduct;
